80f9d74b33ba82ee68587d6ccdfafad865073821
chaolic6505 <chaolic6505@gmail.com>
2022-12-01 22:54:03 -0800

    add buttons and adjust circles

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -4,7 +4,6 @@ import { motion } from 'framer-motion';
 type Props = {};
 
 export default function BackgroundCircles({}: Props) {
-
     return (
         <motion.div
             initial={{
@@ -21,27 +20,17 @@ export default function BackgroundCircles({}: Props) {
             className="relative flex justify-center items-center"
         >
             <div
+                className="absolute border border-slate-500 opacity-50 rounded-full h-[100px] w-[100px] mt-64 md:mt:52 
+            animate-ping"
+            />
+            <div />
+            <div
                 className="absolute border border-slate-500 opacity-50 rounded-full h-[200px] w-[200px] mt-64 md:mt:52 
             animate-ping"
             />
             <div />
             <div
-                className="absolute border border-slate-500 opacity-50 rounded-full h-[300px] w-[300px] mt-64 md:mt:52 
-            animate-ping"
-            />
-            <div />
-            <div
-                className="absolute border-2 border-slate-500  opacity-50 rounded-full h-[500px] w-[500px] mt-64 md:mt:52 
-            animate-ping"
-            />
-            <div />
-            <div
-                className="absolute border border-grayColor opacity-50 h-[510px] w-[510px] md:h-[650px] md:w-[650px] 
-            animate-pulse mt-64 md:mt:52 rounded-full"
-            />
-            <div />
-            <div
-                className="absolute border border-slate-500 opacity-50 rounded-full h-[800px] w-[800px] mt-64 md:mt:52 
+                className="absolute border-2 border-slate-500  opacity-50 rounded-full h-[300px] w-[300px] mt-64 md:mt:52 
             animate-ping"
             />
             <div />
